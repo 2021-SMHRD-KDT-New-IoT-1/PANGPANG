86350fd87cf464f23de0035d60cbebc394f16113
이강건 <thfflem15@naver.com>
2021-12-13 12:23:33 +0900

    프래그먼트 디자인 완료

--- app/src/main/java/com/cjh/finalproject/MainActivity.java
+++ app/src/main/java/com/cjh/finalproject/MainActivity.java
@@ -14,75 +14,19 @@ import com.cjh.finalproject.R;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton led_off;
-    private ImageButton rec_off;
-    private ImageButton fire_off;
-    private ImageButton camera_off;
-    String check = "off";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        led_off = findViewById(R.id.led_off);
-        rec_off = findViewById(R.id.rec_off);
-        fire_off = findViewById(R.id.fire_off);
-        camera_off = findViewById(R.id.camera_off);
+        // 처음부터 1번 화면으로 나오게 만드는 방법
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.layout, new main()).commit();
 
 
-        led_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Drawable bg = led_off.getBackground();
-                if (check == "off") {
-                    bg.setTint(Color.parseColor("#FBC02D"));
-                    check = "on";
-                } else {
-                    bg.setTint(Color.parseColor("#FFFDE4"));
-                    check = "off";
-                }
-            }
-        });
-        rec_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Drawable bg2 = rec_off.getBackground();
-                if (check == "off") {
-                    bg2.setTint(Color.parseColor("#D50000"));
-                    check = "on";
-                } else {
-                    bg2.setTint(Color.parseColor("#FFFDE4"));
-                    check = "off";
-                }
-            }
-        });
-        fire_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Drawable bg3 = fire_off.getBackground();
-                if (check == "off") {
-                    bg3.setTint(Color.parseColor("#D50000"));
-                    check = "on";
-                } else {
-                    bg3.setTint(Color.parseColor("#FFFDE4"));
-                    check = "off";
-                }
-            }
-        });
-        camera_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Drawable bg4 = camera_off.getBackground();
-                if (check == "off") {
-                    bg4.setTint(Color.parseColor("#FBC02D"));
-                    check = "on";
-                } else {
-                    bg4.setTint(Color.parseColor("#FFFDE4"));
-                    check = "off";
-                }
-            }
-        });
+
 
 
     }
