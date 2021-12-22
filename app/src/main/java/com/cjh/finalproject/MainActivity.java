bdfef4efb670fe1e42863137d307c4b767b63e2a
김태석 <paeaned@gmail.com>
2021-12-22 14:59:55 +0900

    디자인 변경

--- app/src/main/java/com/cjh/finalproject/MainActivity.java
+++ app/src/main/java/com/cjh/finalproject/MainActivity.java
@@ -3,13 +3,9 @@ package com.cjh.finalproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -38,11 +34,11 @@ public class MainActivity extends AppCompatActivity {
         bnview.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.tab3) {
+                if (item.getItemId() == R.id.tab2) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.layout, new blackbox()).commit();
                 }else if(item.getItemId() == R.id.tab1){
                     getSupportFragmentManager().beginTransaction().replace(R.id.layout, new main()).commit();
-                }else if(item.getItemId() == R.id.tab2){
+                }else if(item.getItemId() == R.id.tab3){
                     getSupportFragmentManager().beginTransaction().replace(R.id.layout, new setting()).commit();
                 }
                 return true;
