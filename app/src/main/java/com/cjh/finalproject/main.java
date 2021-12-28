30e574029a13820a12eeaf44acaa17f26dbc036e
김태석 <paeaned@gmail.com>
2021-12-28 21:05:45 +0900

    일산화탄소 알람 서비스 추가

--- app/src/main/java/com/cjh/finalproject/main.java
+++ app/src/main/java/com/cjh/finalproject/main.java
@@ -1,7 +1,10 @@
 package com.cjh.finalproject;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
@@ -16,7 +19,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -26,11 +28,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -46,19 +48,16 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.List;
 
-import org.json.JSONException;
-
 
 public class main extends Fragment {
 
     //가스값을 받아올 변수 gas_value
-    private int gas;
 
     private ImageButton btn_led;
-    private ImageButton btn_rec;
+    private ImageButton btn_co;
 
     private TextView tv_led;
-    private TextView tv_rec;
+    private TextView tv_co;
     private TextView tv_progress;
     private TextView tv_COdt;
 
@@ -73,16 +72,22 @@ public class main extends Fragment {
     private Geocoder geocoder;
     private RequestQueue requestQueue;
 
-    double lon;
-    double lat;
+    private double lon;
+    private double lat;
 
-    String check = "off";
-    ProgressBar pb;
-    int max; // 프로그래스바 max 값
-    int speed = 15; // 값 올릴수록 Progress 채워지는 속도 느려짐
+    private String check = "off";
+    private ProgressBar pb;
 
-    int lv1 = 33, lv2 = 66;
-    int coLv1 = Color.parseColor("#00ff62"), coLv2 = Color.parseColor("#fff700"), coLv3 = Color.parseColor("#ff0400");
+    private int max; // 프로그래스바 max 값
+    private int speed = 15; // 값 올릴수록 Progress 채워지는 속도 느려짐
+
+    private int lv1 = 33, lv2 = 66;
+    private int coLv1 = Color.parseColor("#00ff62"), coLv2 = Color.parseColor("#fff700"), coLv3 = Color.parseColor("#ff0400");
+
+    private Drawable bg2;
+    private Boolean check_spf;
+    SharedPreferences spf;
+
 
     final static String ip = "project-student-6.ddns.net/pang_Com";
 
@@ -93,10 +98,10 @@ public class main extends Fragment {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
         btn_led = v.findViewById(R.id.btn_led);
-        btn_rec = v.findViewById(R.id.btn_rec);
+        btn_co = v.findViewById(R.id.btn_co);
 
         tv_led = v.findViewById(R.id.tv_led);
-        tv_rec = v.findViewById(R.id.tv_rec);
+        tv_co = v.findViewById(R.id.tv_co);
         tv_progress = v.findViewById(R.id.tv_progress);
         tv_COdt = v.findViewById(R.id.tv_COdt);
 
@@ -110,8 +115,22 @@ public class main extends Fragment {
 
         geocoder = new Geocoder(getContext());
 
+        bg2 = btn_co.getBackground();
+
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(getContext());
+        }
+
+        spf = getActivity().getSharedPreferences("spf", Context.MODE_PRIVATE);
+
+        check_spf = spf.getBoolean("check",false);
+
+        if (check_spf) {
+            bg2.setTint(Color.parseColor("#D50000"));
+            img_rec.setImageResource(R.drawable.background_cylinder3);
+            tv_co.setText("ON");
+            tv_co.setTextColor(Color.parseColor("#D50000"));
+            check = "on";
         }
 
         // GPS 기능 ---------------------------------------------------------------------------------
@@ -235,12 +254,11 @@ public class main extends Fragment {
             public void onClick(View v) {
                 Drawable bg = btn_led.getBackground();
                 if (check.equals("off")) {
-                    bg.setTint(Color.parseColor("#FBC02D"));
+                    bg.setTint(Color.parseColor("#f0eb54"));
                     img_led.setImageResource(R.drawable.background_cylinder3);
                     tv_led.setText("ON");
-                    tv_led.setTextColor(Color.parseColor("#FBC02D"));
+                    tv_led.setTextColor(Color.parseColor("#f0eb54"));
                     check = "on";
-
                 } else {
                     bg.setTint(Color.parseColor("#FFFFFF"));
                     img_led.setImageResource(R.drawable.background_cylinder2);
@@ -251,24 +269,76 @@ public class main extends Fragment {
             }
         });
 
-        // 녹화기능 버튼 -----------------------------------------------------------------------------
-        btn_rec.setOnClickListener(new View.OnClickListener() {
+        // 일산화탄소 기능 버튼 -----------------------------------------------------------------------------
+        btn_co.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Drawable bg2 = btn_rec.getBackground();
-                if (check == "off") {
-                    bg2.setTint(Color.parseColor("#D50000"));
-                    img_rec.setImageResource(R.drawable.background_cylinder3);
-                    tv_rec.setText("ON");
-                    tv_rec.setTextColor(Color.parseColor("#D50000"));
-                    check = "on";
-                } else {
-                    bg2.setTint(Color.parseColor("#FFFFFF"));
-                    img_rec.setImageResource(R.drawable.background_cylinder2);
-                    tv_rec.setText("OFF");
-                    tv_rec.setTextColor(Color.parseColor("#FFFFFF"));
-                    check = "off";
+                if(check_spf) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("일산화탄소 감시");
+                    builder.setMessage("일산화탄소 감시를 끄겠습니까?");
+                    builder.setPositiveButton("취소", null);
+                    builder.setNegativeButton("끄기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            check_spf = false;
+
+                            SharedPreferences.Editor edit = spf.edit();
+                            edit.putBoolean("check", check_spf);
+                            edit.commit();
+
+                            Toast.makeText(getContext(),"일산화탄소 감시 종료",Toast.LENGTH_SHORT).show();
+
+                            bg2.setTint(Color.parseColor("#FFFFFF"));
+                            img_rec.setImageResource(R.drawable.background_cylinder2);
+                            tv_co.setText("OFF");
+                            tv_co.setTextColor(Color.parseColor("#FFFFFF"));
+
+                            Intent intent = new Intent(getContext(),AlarmService.class);
+                            getActivity().stopService(intent);
+                        }
+                    });
+                    builder.create().show();
+                } else if (!check_spf) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("일산화탄소 감시");
+                    builder.setMessage("일산화탄소 감시를 켜시겠습니까?");
+                    builder.setPositiveButton("취소", null);
+                    builder.setNegativeButton("켜기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            check_spf = true;
+
+                            SharedPreferences.Editor edit = spf.edit();
+                            edit.putBoolean("check", check_spf);
+                            edit.commit();
+                            Toast.makeText(getContext(),"일산화탄소 감시 시작",Toast.LENGTH_SHORT).show();
+
+                            bg2.setTint(Color.parseColor("#D50000"));
+                            img_rec.setImageResource(R.drawable.background_cylinder3);
+                            tv_co.setText("ON");
+                            tv_co.setTextColor(Color.parseColor("#D50000"));
+
+                            Intent serviceIntent = new Intent(getContext(), AlarmService.class);
+                            getActivity().startService(serviceIntent);
+                        }
+                    });
+                    builder.create().show();
                 }
+
+//                if (check == "off") {
+//                    bg2.setTint(Color.parseColor("#D50000"));
+//                    img_rec.setImageResource(R.drawable.background_cylinder3);
+//                    tv_co.setText("ON");
+//                    tv_co.setTextColor(Color.parseColor("#D50000"));
+//                    check = "on";
+//                } else {
+//                    bg2.setTint(Color.parseColor("#FFFFFF"));
+//                    img_rec.setImageResource(R.drawable.background_cylinder2);
+//                    tv_co.setText("OFF");
+//                    tv_co.setTextColor(Color.parseColor("#FFFFFF"));
+//                    check = "off";
+//                }
             }
         });
 
@@ -309,32 +379,32 @@ public class main extends Fragment {
         public void run() {
 
             // while (true) {
-                
-                try {
 
-                    int loop = 0;
-                    if (max < 50) {
-                        loop = 33;
-                    } else if (max < 100) {
-                        loop = 66;
-                    } else {
-                        loop = 100;
-                    }
+            try {
 
-                    Log.v("error", String.valueOf(max));
-                    for (int i = 0; i < loop; i++) {
-                        Message msg = new Message();
-                        msg.arg1 = i;
-
-                        Thread.sleep(speed);
-                        handler.sendMessage(msg);
-                    }
-
-                    //30초 지난 후
-                    Thread.sleep(1000 * 5);
-                } catch (InterruptedException e) {
-                    Log.v("gas", e.toString());
+                int loop = 0;
+                if (max < 50) {
+                    loop = 33;
+                } else if (max < 100) {
+                    loop = 66;
+                } else {
+                    loop = 100;
                 }
+
+                Log.v("error", String.valueOf(max));
+                for (int i = 0; i < loop; i++) {
+                    Message msg = new Message();
+                    msg.arg1 = i;
+
+                    Thread.sleep(speed);
+                    handler.sendMessage(msg);
+                }
+
+                //~~초 지난 후
+                Thread.sleep((1000 * 5));
+            } catch (InterruptedException e) {
+                Log.v("gas", e.toString());
+            }
 
             // }
 
@@ -342,26 +412,23 @@ public class main extends Fragment {
         }
     }
 
+    // 일산화탄소 progress 바 초기화 쓰레드
     public class TimeThread extends Thread {
         // extends Thread 했기 때문에~ 지금부터 Thread!
 
-        private int start; // 해당 Thread가 몇초부터 시작할 건지 전달받을거임~!
+        private int start;
 
         public TimeThread(int start) {
             this.start = start;
         }
 
-        // Thread가 시작하면 실행되는 메소드
         @Override
         public void run() {
-            // 30부터 0까지 1씩 감소해야함!
-            // 뭘 써야할까? for문!
             while (true) {
 
                 responsePpm();
-                // Thread 쉬게하는 법~~
                 try {
-                    Thread.sleep(1000 * 5);
+                    Thread.sleep((1000 * 5));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -418,7 +485,6 @@ public class main extends Fragment {
                         try {
                             JSONObject jObject = new JSONObject(response);
                             max = Integer.parseInt(jObject.getString("gas"));
-                            Log.v("gas", Integer.toString(gas));
 
                             Log.v("gas", "max: " + max);
 
