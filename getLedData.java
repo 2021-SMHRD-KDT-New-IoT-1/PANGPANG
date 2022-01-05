826b8483efaccaa0bb3422400fa053de0b806cbc
임주현 <91045325+toyzzzz@users.noreply.github.com>
2022-01-05 11:43:13 +0900

    Create getLedData.java

---
+++ getLedData.java
@@ -0,0 +1,28 @@
+package upload;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/getLedData")
+public class getLedData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		fileUploadMgr getLedData = new fileUploadMgr();
+		int led = getLedData.getLed();
+		PrintWriter out = response.getWriter();
+//		String move = new Gson().toJson(led);
+ 		out.print("{\"move\":\"" + led + "\"}");
+//		out.print("{\"move\":\"1\"}");
+		
+		
+	}
+
+}
